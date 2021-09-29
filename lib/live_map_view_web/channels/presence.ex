1b721df9c6e444bd36760cb953a53e4202d7190d
thehaigo <sirannha@gmail.com>
2021-09-29 22:15:42 +0900

    impl presence util function

--- lib/live_map_view_web/channels/presence.ex
+++ lib/live_map_view_web/channels/presence.ex
@@ -7,4 +7,25 @@ defmodule LiveMapViewWeb.Presence do
   """
   use Phoenix.Presence, otp_app: :live_map_view,
                         pubsub_server: LiveMapView.PubSub
+
+  alias LiveMapViewWeb.Presence
+
+  def list_presence(topic) do
+    topic
+    |> Presence.list
+    |> Enum.map(fn {_user_id, data} -> extract_metadata(data) end)
+  end
+
+  def update_presence(pid, topic, key, payload) do
+    metas =
+      Presence.get_by_key(topic, key)
+      |> extract_metadata()
+      |> Map.merge(payload)
+
+    Presence.update(pid, topic, key, metas)
+  end
+
+  def extract_metadata(data) do
+    data |> Map.get(:metas) |> List.first
+  end
 end
